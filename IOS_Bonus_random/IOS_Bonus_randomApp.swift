f683f58dd46ca1420008dcfc27120d0257930078
Eric's MB <eric17618@gmail.com>
2020-12-15 16:31:56 +0800

    Initial Commit

---
+++ IOS_Bonus_random/IOS_Bonus_randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Bonus_randomApp.swift
+//  IOS_Bonus_random
+//
+//  Created by 翁星宇 on 2020/12/15.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Bonus_randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
